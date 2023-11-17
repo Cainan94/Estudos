e199b7de43711508a0859dec8f0a8362386943ef
cainan.tornich <cainan.tornich@betha.com.br>
2023-11-17 09:29:25 -0300

    finalizado modulo criacionais.

--- Padrões de Projeto em JAVA/creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
+++ Padrões de Projeto em JAVA/creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.cod3r.prototype.animation.model.Person;
+import br.com.cod3r.prototype.animation.model.PersonSamples;
 
 public class Client {
 	private static List<Person> frames = new ArrayList<Person>();
@@ -25,8 +26,34 @@ public class Client {
 		System.out.println("* ");
 		System.out.print("**********************");
 	}
+
+	public static Person addLeft(Person person) throws CloneNotSupportedException {
+		Person p = (Person)person.clone();
+		p.left();
+		frames.add(p);
+		return p;
+	}
+
+	public static Person addRight(Person person) throws CloneNotSupportedException {
+		Person p = (Person)person.clone();
+		p.right();
+		frames.add(p);
+		return p;
+	}
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException, CloneNotSupportedException {
+		PersonSamples personSamples = new PersonSamples();
+		Person person = personSamples.get("stickMan");
+		frames.add(person);
+		for(int i = 0; i < 6; i++){
+		   person = addLeft(person);
+		}
+		for(int i = 0; i < 12; i++){
+			person = addRight(person);
+		}
+		for(int i = 0; i < 6; i++){
+			person = addLeft(person);
+		}
+		animate();
 	}
 }
--- Padrões de Projeto em JAVA/creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
+++ Padrões de Projeto em JAVA/creational-patterns/src/main/java/br/com/cod3r/prototype/functionalCloneBuilder/model/User.java
@@ -1,15 +1,19 @@
 package br.com.cod3r.prototype.functionalCloneBuilder.model;
 
 public class User {
-	private String name;
-	private Integer age;
-	private Address address;
+	private final String name;
+	private final Integer age;
+	private final Address address;
 	
 	public User(String name, Integer age, Address address) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.address = address;
+	}
+
+	public Builder cloneBuilder(){
+		return new Builder(name,age,address);
 	}
 	
 	public String getName() {
@@ -27,5 +31,37 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+	public static class Builder{
+		private String name;
+		private Integer age;
+		private Address address;
+
+		private Builder(String name, Integer age, Address address){
+			this.name = name;
+			this.age = age;
+			this.address = address;
+		}
+
+		public Builder Name(String name){
+			this.name = name;
+			return this;
+		}
+
+		public Builder Age(Integer age){
+			this.age = age;
+			return this;
+		}
+
+		public Builder Address(Address address){
+			this.address = address;
+			return this;
+		}
+
+		public User build(){
+			return new User(name,age,address);
+		}
+
+
 	}
 }
